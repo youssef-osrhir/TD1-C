bc549c3edab23c66e3902e84fce6f9c52d49dae5
Youssef Osrhir <Youssef Osrhir>
2020-03-17 16:45:11 +0100

    exo9

---
+++ td1-9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    float A;
+
+    scanf("%f", &A);
+
+    float x = A;
+
+    while (abs(x - sqrt(A)) > 1e-3)
+    {
+        x = 0.5 * (x + A / x);
+        printf("%f\n", x - sqrt(A));
+    }
+
+    printf("l'approximation de la racine de %f est: %f\n", A, x);
+}
