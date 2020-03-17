e55d56ebf6fc1c20a4dc283eceac2199993f0755
Youssef Osrhir <Youssef Osrhir>
2020-03-17 15:16:20 +0100

    exo 3

---
+++ td1-31.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i, n, a, b, c;
+    a = 1;
+    b = 1;
+    i = 2;
+
+    scanf("%d", &n);
+    while (n < 2)
+    {
+        scanf("%d", &n);
+    }
+
+    while (i <= n)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+        i++;
+    }
+    printf("%d \n", c);
+}
---
+++ td1-32.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b, c, n;
+    a = 1;
+    b = 1;
+    int i = 2;
+
+    scanf("%d", &n);
+    while (n < 2)
+    {
+        scanf("%d", &n);
+    }
+
+    while (i <= n)
+    {
+        c = a + b;
+        printf("U%d = %d\n", i, c);
+        a = b;
+        b = c;
+        i++;
+    }
+}
